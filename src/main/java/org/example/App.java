0004918bb45ae94680f9eb1a58b8e73b6223b42a
Andrey <aimakar@mail.ru>
2023-02-10 12:08:26 +0300

    Commit 1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,13 @@
 package org.example;
 
+
+import org.example.model.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.security.Permission;
+
 /**
  * Hello world!
  *
@@ -8,6 +16,24 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+
+        Session session = sessionFactory.getCurrentSession();
+        try {
+            session.beginTransaction();
+
+            Person person = new Person("Bob", 32);
+            Person person2 = new Person("Tyna",21);
+            Person person3 = new Person("Grendel", 18);
+            session.save(person);
+            session.save(person2);
+            session.save(person3);
+
+            session.getTransaction().commit();
+        } finally {
+            sessionFactory.close();
+        }
     }
 }
