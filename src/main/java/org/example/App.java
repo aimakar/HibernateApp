e9bdfbd1368a21fadc8d41a7d49dd07069b39019
Andrey <aimakar@mail.ru>
2023-02-17 13:47:55 +0300

    Commit 3

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,12 +1,14 @@
 package org.example;
 
 
+import org.example.model.Item;
 import org.example.model.Person;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import java.security.Permission;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -17,23 +19,25 @@ public class App
 {
     public static void main( String[] args )
     {
-        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class)
+            .addAnnotatedClass(Item.class);
 
         SessionFactory sessionFactory = configuration.buildSessionFactory();
-
         Session session = sessionFactory.getCurrentSession();
         try {
             session.beginTransaction();
 
-            session.createQuery("delete from Person where age< 30").executeUpdate();
+            Person person = new Person("Person3", 23);
+            person.addItem(new Item("item1"));
+            person.addItem(new Item("item2"));
+            person.addItem(new Item("item3"));
 
-//            for (Person person : people)
-//                System.out.println(person.toString());
+            session.save(person);
 
             session.getTransaction().commit();
-
         } finally {
             sessionFactory.close();
         }
     }
+
 }
--- src/main/java/org/example/model/Person.java
+++ src/main/java/org/example/model/Person.java
@@ -1,59 +1,88 @@
 package org.example.model;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.Cascade;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * @author Andrey Makarov on 13.01.2023
+ * @author Andrey Makarov on 14.02.2023
  */
 @Entity
 @Table(name = "Person")
 public class Person {
 
-    @Id
-    @Column(name="id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
 
-    @Column(name="name")
-    private String name;
+	@Column(name = "name")
+	private String name;
 
-    @Column(name="age")
-    private int age;
+	@Column(name = "age")
+	private int age;
 
-    public Person() {
-    }
+	@OneToMany(mappedBy = "owner")
+	@Cascade(org.hibernate.annotations.CascadeType.SAVE_UPDATE)
+	private List<Item> items;
 
-    public Person(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
+	public Person() {
 
-    public int getId() {
-        return id;
-    }
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	public Person( String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public int getId() {
+		return id;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    public int getAge() {
-        return age;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setAge(int age) {
-        this.age = age;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String toString() {
-        return this.name + ", " + this.age;
-    }
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public List<Item> getItems() {
+		return items;
+	}
+
+	public void setItems(List<Item> items) {
+		this.items = items;
+	}
+
+	public void addItem(Item item) {
+		if (this.items == null)
+			this.items = new ArrayList<>();
+		this.items.add(item);
+		item.setOwner(this);
+	}
+
+	@Override
+	public String toString() {
+		return "Person{" +
+			"id=" + id +
+			", name='" + name + '\'' +
+			", age=" + age +
+			'}';
+	}
 
 }
