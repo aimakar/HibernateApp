3695f09a7f2a6f1cd2d5887bcf87f1b49ae3ab16
Andrey <aimakar@mail.ru>
2023-02-18 14:40:46 +0300

    Commit 5

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,11 +1,15 @@
 package org.example;
 
 
-import org.example.model.Passport;
-import org.example.model.Person;
+import org.example.model.Actor;
+import org.example.model.Movie;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Hello world!
@@ -15,20 +19,27 @@ public class App
 {
     public static void main( String[] args )
     {
-        Configuration configuration = new Configuration().addAnnotatedClass(Person.class)
-            .addAnnotatedClass(Passport.class);
+        Configuration configuration = new Configuration().addAnnotatedClass(Actor.class)
+            .addAnnotatedClass(Movie.class);
 
         SessionFactory sessionFactory = configuration.buildSessionFactory();
-        Session session = sessionFactory.getCurrentSession();
-        try {
+
+        try (sessionFactory) {
+            Session session = sessionFactory.getCurrentSession();
             session.beginTransaction();
 
-            Person person = session.get(Person.class, 1);
-            session.remove(person);
+            Actor actor = session.get(Actor.class, 2);
+            System.out.println(actor.getMovies());
+
+            Movie movieToRemove = actor.getMovies().get(0);
+
+            actor.getMovies().remove(0);
+            movieToRemove.getActors().remove(actor);
+
+
+
 
             session.getTransaction().commit();
-        } finally {
-            sessionFactory.close();
         }
     }
 
